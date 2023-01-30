d9e21653ffa8f37a607eeea7faa293909d1eb65d
Mogwai <diane.roberg@gmail.com>
2023-01-30 22:44:22 +0100

    premier commit

---
+++ fonctions.js
@@ -0,0 +1,104 @@
+/*  /////////////           TABLEAU HTML            /////////// */
+
+function ajouterRangee(item, tab){  // fonction qui ajoute et remplit une rangée
+                                // du tableau HTML avec un item entré
+
+    let rangee = document.createElement("tr");  // création de la rangée
+    let props = ["nom", "ref", "quantite"]; // liste des propriétés de l'item
+
+    for (let i = 0; i < 3; i++) {
+        let cellule = document.createElement("td");  // création de la cellule
+        cellule.setAttribute("id", `table-${props[i]}-${item[`${props[0]}`]}`);  // la cellule suit la convention de nommage "table-propriété-nom"
+        cellule.innerText= item[`${props[i]}`];
+        rangee.appendChild(cellule);
+    }   // FIN POUR
+    return rangee;
+}   // FIN FX
+
+
+function affichageStock (stock, tab) {  //récupère les données du stock utilisateur afin d'afficher 
+                                        // le tableau HTML du stock
+
+    stock.forEach(e => { // pour chaque item e dans le stock
+        updateAffichageStock(e, tab);
+    });   
+
+    return;
+}
+
+
+function updateAffichageStock(item, tab){   // quand on ajoute un item au stock,
+                                        // cette fonction va servir à màj le tableau HTML
+    tab.appendChild(ajouterRangee(item, tab)); // on le met dans une rangée qu'on ajoute au tableau HTML
+    return;
+}
+
+
+
+/*  //////////              STOCK UTILISATEUR       ///////////////////    */
+
+function updateStockUser(stock, item) {
+
+    // on vérifie s'il y a déjà ce produit dans le stock
+    for (let i=0; i<stock_user.length; i++) {
+
+        if (stock[i].nom === item.nom){
+
+            // si oui, on va le mettre à jour
+            stock[i].quantite += item.qte;
+            return stock;
+        }
+    }
+
+    // si le produit n'est pas encore dans le stock, on va l'ajouter
+    stock.push(item);
+
+    return stock;
+}
+
+
+/*      /////////////////////       COMMANDE             ////////////////////// */
+
+
+
+function traiterCmd(nom, ref, qte, stock, tab) {
+    let total=0; 
+
+    let item_input = {  // création d'un objet JSON de l'item commandé
+        "nom" : nom,
+        "ref" : ref,
+        "quantite" : qte
+    };
+
+    // envoi de l'item dans le stockage local
+    localStorage.setItem('item'+nb_item, JSON.stringify(item_input));
+
+    nb_item++;  // on incrémente le numéro d'item pour la suite de la commande
+
+    //  update du stock actuel
+    stock=updateStockUser(stock_user, item_input);
+
+    // màj du tableau HTML du stock
+    updateAffichageStock(item_input, tab);
+
+    return;
+}
+
+
+function checkCmd(ref, qte) {   
+    // vérification des entrées
+    if (qte == 0 || qte == "") {
+        window.alert("Veuillez entrer une quantité valide");
+        return 0;
+    } else if (ref == ""){
+        window.alert ("Veuillez entrer une référence valide");
+        return 0;
+    }
+    else {
+        return 1;
+    }
+    return 'erreur 2';
+}
+
+
+
